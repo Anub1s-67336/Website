9718eedfea58e3eec7a11f425890f73cdc192b62
Valerik <zakharyanzakar@gmail.com>
2026-02-12 16:14:49 +0500

    добавил database setup, models and schemas

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,69 +1,46 @@
-from fastapi import FastAPI, HTTPException
-from typing import Optional, List, Dict
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, Path, Query, Body, Depends
+from typing import Optional, List, Dict, Annotated
+from sqlalchemy.orm import Session
+from models import Base, User, Post
+from database import engine, session_local
+from schemas import UserCreate, User as DbUser, PostCreate, PostResponse
+
 
 app = FastAPI()
 
+Base.metadata.create_all(bind = engine)
 
-class User(BaseModel):
-    id: int
-    name: str
-    age: int
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
 
+@app.post("/users/", response_model=DbUser)
+def create_user(user: UserCreate, db: Session = Depends(get_db)) -> DbUser:
+    db_user = User(name = user.name,age = user.age )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
 
-class Post(BaseModel):
-    id: int
-    title: str
-    body: str
-    avtor: User
-
-
-users = [
-    {'id': 1, 'name': "Valerik", 'age': 15},
-    {'id': 2, 'name': "Jonsh", 'age': 17},
-    {'id': 3, 'name': "Mark", 'age': 45}
-
-]
+    return db_user
 
 
-posts = [
-    {'id': 1, 'title': 'News 1', 'body': 'Text 1', 'avtor': users[1]},
-    {'id': 2, 'title': 'News 2', 'body': 'Text 2', 'avtor': users[2]},
-    {'id': 3, 'title': 'News 3', 'body': 'Text 3', 'avtor': users[0]}
-]
 
-@app.get("/")
-def root():
-    return {"message": "Привет мир"}
+@app.post("/posts/", response_model=PostResponse)
+def create_post(post: PostCreate, db: Session = Depends(get_db)) -> Post:
+    db_user = db.query(User).filter(User.id == post.author_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='User not found')
+    db_post = Post(title = post.title, body = post.body, author_id = post.author_id )
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
 
-@app.get("/items")
-def items() -> List[Post]:
-    return   [Post(**post) for post in posts]
+    return db_post
 
-@app.get("/items/{id}")
-def get_items(id: int) -> Post:
-    for post in posts:
-        if post['id'] == id:
-            return Post(**post)
-    raise HTTPException(status_code=404, detail='post dont found')
 
-@app.get("/search")
-def search(post_id: Optional[int] = None) -> Dict[str, Optional[Post]]:
-    if post_id:
-         for post in posts:
-            if post['id'] == post_id:
-                return {"dats": Post(**post)}
-         raise HTTPException(status_code=404, detail='post dont found')
-    else:
-        return{"data": None}    
-
-@app.get("/Users")    
-def get_all_users() -> List[User]:
-    return [User(**user)for user in users]
-
-@app.get("/Users/{id}")
-def get_user(id: int) -> User:
-     for user in users:
-        if user['id'] == id:
-            return User(**user)
-     raise HTTPException(status_code=404, detail='post dont found')
+@app.get("/posts/", response_model=List[PostResponse])
+def get_post(db: Session = Depends(get_db)):
+    return db.query(Post).all()
